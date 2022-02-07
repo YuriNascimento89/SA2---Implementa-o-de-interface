35f1cade02b8738a6727f99a34bcec8ab02e6688
Yuri Nascimento <yuripuc@hotmail.com>
2022-02-07 10:51:07 -0300

    inclusão bootstrap e Javascript

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function mostrarPopup(){
+    window.alert( "hello world")
+}
+
+let email = document.getElementById("campo-email");
+
+function enviarEmail(){
+let emailDigitado = email.value;
+console.log(emailDigitado)
+}
