d5fc5939f0e0f5fba1e9ed0d5e0d036b853203d3
Yuri Nascimento <yuripuc@hotmail.com>
2022-02-15 21:19:24 -0300

    Modificação Javascript e JQuery

--- js/script.js
+++ js/script.js
@@ -1,6 +1,37 @@
-function mostrarPopup(){
-    window.alert( "hello world")
-}
+$(document).ready(function(){
+
+    $("#barras").click(function(){
+        $("menu").toggleClass("#menu-ativo") //faz a mudança entre if e else. tudo que está no codigo abaixo.
+    })
+})
+
+$(document).ready(function(){
+    $("#carrosel img:eq(0)").addClass("banner-ativo").show()
+
+})
+
+setInterval(slide,1000)
+function slide(){
+
+
+    if($(".banner-ativo").next().lenght){
+        $(".banner.ativo").removeClass("banner-ativo").hide().next().addClass("banner-ativo").show()
+    }else{
+        $(".banner-ativo").removeClass().hide()
+        $("#carrosel img:eq(0)").addClass("banner-ativo").show()
+    }
+    }
+
+      // if($("#menu").hasClass("#menu-ativo")){
+       //     $("menu").removeClass("#menu-ativo")
+      // }else{
+       //    $("#menu").addClass("menu-ativo")
+   //    }
+   // })
+//})
+//function mostrarPopup(){
+   // window.alert( "Fala Gamer!!!")
+//}
 
 let email = document.getElementById("campo-email");
 
